ac429161b7f8bf08f57ec0efa0c2e2c615252662
Peng Sun <pesun@ipp2-0913.nvidia.com>
2023-06-27 20:26:25 +0000

    Fixed device memory leakage in PMLs for multiGPU

--- src/fdtd.cu
+++ src/fdtd.cu
@@ -2268,8 +2268,17 @@ void fdtd::FDTD::block_CUDA_multigpu_init()
 
 void fdtd::FDTD::block_CUDA_multigpu_free()
 {
+    int i0t{}, j0t{}, k0t{}, It{}, Jt{}, Kt{};
     for(int device_id=0; device_id<_gpus_count; device_id++){
         cudaSetDevice(device_id);
+
+        k0t = _kpar_list_host[device_id]->k0;
+        j0t = _kpar_list_host[device_id]->j0;
+        i0t = _kpar_list_host[device_id]->i0;
+        Kt = _kpar_list_host[device_id]->K;
+        Jt = _kpar_list_host[device_id]->J;
+        It = _kpar_list_host[device_id]->I;
+
         gpuErrchk(cudaFree(_kpar_list_host[device_id]->bc));
         // sources
         gpuErrchk(cudaFree(_kpar_list_host[device_id]->i0s));
@@ -2323,32 +2332,43 @@ void fdtd::FDTD::block_CUDA_multigpu_free()
         gpuErrchk(cudaFree(_kpar_list_host[device_id]->cEy));
         gpuErrchk(cudaFree(_kpar_list_host[device_id]->cEz));
 
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Exy0));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Exy1));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Exz0));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Exz1));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Eyx0));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Eyx1));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Eyz0));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Eyz1));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Ezx0));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Ezx1));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Ezy0));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Ezy1));
+        if(k0t < _w_pml_x0){
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Eyx0));
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Ezx0));
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hyx0));
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hzx0));
+        }
+        if(k0t+Kt>_Nx-_w_pml_x1){
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Eyx1));
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Ezx1));
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hyx1));
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hzx1));
+        }
 
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hxy0));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hxy1));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hxz0));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hxz1));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hyx0));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hyx1));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hyz0));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hyz1));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hzx0));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hzx1));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hzy0));
-        gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hzy1));
-
+        if(j0t<_w_pml_y0){
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Exy0));
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Ezy0));
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hxy0));
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hzy0));
+        }
+        if(j0t+Jt>_Ny-_w_pml_y1){
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Exy1));
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Ezy1));
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hxy1));
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hzy1));
+        }
+        if(i0t<_w_pml_z0){
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Exz0));
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Eyz0));
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hxz0));
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hyz0));
+        }
+        if(i0t+It>_Nz-_w_pml_z1){
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Exz1));
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Eyz1));
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hxz1));
+            gpuErrchk(cudaFree(_kpar_list_host[device_id]->pml_Hyz1));
+        }
         // gpuErrchk(cudaFree(_kpar_list[device_id]));
     }
 }
